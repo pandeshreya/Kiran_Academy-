8fa6aa8081e921f58539a4d00e506bf4f16e75f6
pandeshreya <pandeshreya2006@gmail.com>
2026-06-05 15:34:23 +0530

    Add files via upload

---
+++ june4_HW.py
@@ -0,0 +1,31 @@
+#HW
+#create dictionary movies_db/webseries
+#minnimum 5 movies 
+
+movies_db = {}
+
+# movie 1 
+cast1 = ["Ranveer Singh", "Akshay Khanna", "SD", "Sara Arjun"]
+movies_db["Dhurandhar"] = cast1
+
+# movie 2
+cast2 = ["Bill Skarsgård", "Jaeden Martell", "Finn Wolfhard", "Sophia Lillis"]
+movies_db["It Chapter 1"] = cast2
+
+# Movie 3
+cast3 = ["Dylan O'Brien", "Kaya Scodelario", "Thomas Brodie-Sangster", "Ki Hong Lee"]
+movies_db["Maze Runner"] = cast3
+
+# Movie 4
+cast4 = ["Allison Williams", "Violet McGraw", "Amie Donald", "Jenna Davis"]
+movies_db["Megan"] = cast4
+
+# Movie 5
+cast5 = ["Lindsay Lohan", "Rachel McAdams", "Lacey Chabert", "Amanda Seyfried"]
+movies_db["Mean Girls"] = cast5
+
+# Movie 6
+cast6 = ["Matthew McConaughey", "Anne Hathaway", "Jessica Chastain", "Michael Caine"]
+movies_db["Interstellar"] = cast6
+
+print(movies_db)
